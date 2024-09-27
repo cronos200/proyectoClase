046a9ca33121de0b61529c6fc175fee8e14e4307
Desarrollo <jhonjdv2022@outlook.com>
2024-09-26 19:13:26 -0500

    ejemplos de creacion de arreglos

---
+++ script.js
@@ -0,0 +1,15 @@
+// los arrglos son un espacio de memoria que almacena multiple valores 
+//y pueden ser cualquier tipo de datos
+let numero = 23
+let variasCosas = [23, true, "jaime", function(){},[], numero] // podemos introducir cualquier dato
+let otroArreglo = [] // arreglo vacio
+
+console.log(variasCosas[0] * variasCosas[5])
+console.log(variasCosas[1])
+console.log(variasCosas[3])
+console.log(otroArreglo)
+
+
+// a conciderar el tamaño y la posicion
+//tamaño --> hace referencia a la camtidad de elementos dentro del arreglo
+//posicion --> hace referencia a la ccantidad de elementos -1
